bea42d359ab66c8c33956b0d7b6fde4bd0e3b2ff
Grzegorz Żur <grzegorz.zur@gmail.com>
2022-07-13 00:01:55 +0200

    Loading translations

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let translations = {};
+
+class LanguageChange extends CustomEvent {
+
+ static ID = "language-change";
+  
+ code;
+
+ constructor(code) {
+  super(LanguageChange.ID, { detail: { code } });
+ }
+
+}
+ 
+async function language(code) {
+ const response = await fetch(`${code}.json`);
+ translations = await response.json();
+ document.dispatchEvent(new LanguageChange(code));
+} 
+
 class Selector extends HTMLElement {
 
  static ID = 0;
@@ -10,8 +30,7 @@ class Selector extends HTMLElement {
   console.log(this.id);
   const content = document.querySelector("#lang-selector").content.cloneNode(true);
   content.querySelector("select").addEventListener("change", (event) => this.select(event));
-  document.addEventListener("lang-change", (event) => this.change(event));
-  document.addEventListener("lang-changed", (event) => this.changed(event));
+  document.addEventListener(LanguageChange.ID, (event) => this.change(event));
   const style = document.querySelector("#style").cloneNode(true);
   const shadow = this.attachShadow({ mode: "open" });
   shadow.appendChild(style);
@@ -20,18 +39,12 @@ class Selector extends HTMLElement {
 
  select(event) {
   console.log(this.id, "select");
-  const change = new CustomEvent("lang-change", { detail: { id: this.id, value: event.target.value } });
-  change.lang = event.target.value;
-  document.dispatchEvent(change);
+  language(event.target.value);
  }
 
  change(event) {
   console.log(this.id, "change", event.detail);
-  this.shadowRoot.querySelector("select").value = event.detail.value;
- }
-
- changed(event) {
-  console.log(this.id, "changed", event.detail);
+  this.shadowRoot.querySelector("select").value = event.detail.code;
  }
 
 }
